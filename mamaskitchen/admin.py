403304f631eed77cf0fa177fc014e30d9e546020
Peter Liu <onhout@gmail.com>
2016-10-23 17:31:24 -0700

    added initial menu field

--- mamaskitchen/admin.py
+++ mamaskitchen/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import MenuItem, MenuPicURL
 # Register your models here.
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = MenuPicURL
+    extra = 1
+
+
+class MenuItemModelAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "amount", "created_at")
+    inlines = [ChoiceInLine]
+
+    class Meta():
+        model = MenuItem
+        
+admin.site.register(MenuItem, MenuItemModelAdmin)
+admin.site.register(MenuPicURL)
--- mamaskitchen/models.py
+++ mamaskitchen/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    amount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+
+class MenuPicURL(models.Model):
+    itemurl = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
