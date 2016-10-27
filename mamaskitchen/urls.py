1b231a95991b3b5c6bd63b025b6bbf8db86de8ce
Peter Liu <onhout@gmail.com>
2016-10-27 15:19:54 -0700

    urls and views now spitting objects out correctly

--- mamaskitchen/urls.py
+++ mamaskitchen/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
+from .views import Index, About, TypeOfFood
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^type/(?P<type_id>[0-9]+)$', views.foodtype),
-    url(r'^about$', views.about, name='about'),
+    url(r'^$', Index.as_view(), name='index'),
+    url(r'^type/(?P<type_id>[0-9]+)$', TypeOfFood.as_view()),
+    url(r'^about$', About.as_view(), name='about'),
 ]
--- mamaskitchen/views.py
+++ mamaskitchen/views.py
@@ -6,19 +6,48 @@ from django.views import generic
 # Create your views here.
 
 
-def index(request):
-    try:
-        p = FoodType.objects.all()
-    except FoodType.DoesNotExist:
-        raise Http404('Does not exist')
-    return render(request, 'index.html', {'types': p})
+class Menu(generic.ListView):
+    context_object_name = 'types'
+
+    def get_queryset(self):
+        return FoodType.objects.all()
+
+    # def get_queryset(self):
+    #     try:
+    #         p = FoodType.objects.all()
+    #     except FoodType.DoesNotExist:
+    #         raise Http404('please check your database')
+    #     return
 
 
-def foodtype(request, type_id):
-    foodrelationship = get_list_or_404(FoodItemRelationship, type=type_id)
-    item = MenuItem.objects.filter(items__in=foodrelationship).values().order_by('id')
-    return render(request, 'menu/menu.html', {'food': item})
+class Index(Menu):
+    template_name = 'index.html'
 
 
-def about(request):
-    return render(request, 'about/about.html', {})
+# def index(request):
+#     try:
+#         p = FoodType.objects.all()
+#     except FoodType.DoesNotExist:
+#         raise Http404('Does not exist')
+#     return render(request, 'index.html', {'types': p})
+
+class TypeOfFood(Menu):
+    template_name = 'menu/menu.html'
+    # getting the menu bar
+
+    def get_context_data(self, **kwargs):
+        context = super(TypeOfFood, self).get_context_data(**kwargs)
+        foodrelationship = get_list_or_404(FoodItemRelationship, type_id=self.kwargs['type_id'])
+        context['food'] = MenuItem.objects.filter(items__in=foodrelationship).values().order_by('id')
+        context['types'] = FoodType.objects.all()
+        return context
+
+
+class About(Menu):
+    template_name = 'about/about.html'
+
+# def foodtype(request, type_id):
+#     foodrelationship = get_list_or_404(FoodItemRelationship, type=type_id)
+#     item = MenuItem.objects.filter(items__in=foodrelationship).values().order_by('id')
+#     return render(request, 'menu/menu.html', {'food': item})
+
